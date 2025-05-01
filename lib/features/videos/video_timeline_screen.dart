f4cb84969c6bbe0ccc9dd7c9f138b66da00e6d3d
insu <hinsu93@gmail.com>
2025-05-01 13:32:24 +0900

    #7.3 Video Player

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -13,19 +13,24 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
 
   final PageController _pageController = PageController();
 
-  List<Color> colors = [Colors.blue, Colors.red, Colors.yellow, Colors.teal];
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
     if (page == _itemCount - 1) {
       _pageController.animateToPage(
         page,
-        duration: Duration(milliseconds: 100),
-        curve: Curves.linear,
+        duration: _scrollDuration,
+        curve: _scrollCurve,
       );
       _itemCount = _itemCount + 4;
-      colors.addAll([Colors.blue, Colors.red, Colors.yellow, Colors.teal]);
+
       setState(() {});
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
   }
 
   @override
@@ -35,7 +40,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => VideoPost(),
+      itemBuilder:
+          (context, index) => VideoPost(onVideoFinished: _onVideoFinished),
     );
   }
 }
