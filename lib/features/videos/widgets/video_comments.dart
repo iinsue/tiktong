871609bd20ce9b38fb030b0585cf662503d258cb
insu <hinsu93@gmail.com>
2025-05-01 22:01:57 +0900

    #8.2 Add Comment

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -17,7 +17,10 @@ class _VideoCommentsState extends State<VideoComments> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
+      height: size.height * 0.7,
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(Sizes.size14),
@@ -35,73 +38,99 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-            horizontal: Sizes.size16,
-          ),
-          separatorBuilder: (context, index) => Gaps.v20,
-          itemCount: 10,
-          itemBuilder:
-              (context, index) => Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.deepPurple,
-                    foregroundColor: Colors.white,
-                    child: Text("인수"),
-                  ),
-                  Gaps.h10,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "인수",
-                          style: TextStyle(
-                            fontSize: Sizes.size14,
-                            fontWeight: FontWeight.bold,
+        body: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size10,
+                horizontal: Sizes.size16,
+              ),
+              separatorBuilder: (context, index) => Gaps.v20,
+              itemCount: 10,
+              itemBuilder:
+                  (context, index) => Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.deepPurple,
+                        foregroundColor: Colors.white,
+                        child: Text("인수"),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "인수",
+                              style: TextStyle(
+                                fontSize: Sizes.size14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Gaps.v3,
+                            const Text(
+                              "That's not it I've seen the same thing but also in a cave.",
+                            ),
+                          ],
+                        ),
+                      ),
+                      Gaps.h10,
+                      Column(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
                             color: Colors.grey.shade500,
                           ),
-                        ),
-                        Gaps.v3,
-                        const Text(
-                          "That's not it I've seen the same thing but also in a cave.",
-                        ),
-                      ],
-                    ),
-                  ),
-                  Gaps.h10,
-                  Column(
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.heart,
-                        size: Sizes.size20,
-                        color: Colors.grey.shade500,
-                      ),
-                      Gaps.v2,
-                      Text(
-                        "52.2K",
-                        style: TextStyle(color: Colors.grey.shade500),
+                          Gaps.v2,
+                          Text(
+                            "52.2K",
+                            style: TextStyle(color: Colors.grey.shade500),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+            ),
+            Positioned(
+              bottom: 0,
+              width: size.width,
+              child: BottomAppBar(
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white,
+                      child: const Text("인수"),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Write a comment...",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(Sizes.size12),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey.shade200,
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: Sizes.size12,
+                            horizontal: Sizes.size10,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 18,
-                backgroundColor: Colors.deepPurple,
-                foregroundColor: Colors.white,
-                child: const Text("인수"),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
