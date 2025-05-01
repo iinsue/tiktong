28f48eac2365089033ebf89bfc93866811cbf103
insu <hinsu93@gmail.com>
2025-05-01 14:05:22 +0900

    #7.4 Video 가 화면 100% 일 때 재생

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
+  final int index;
 
-  const VideoPost({super.key, required this.onVideoFinished});
+  const VideoPost({
+    super.key,
+    required this.index,
+    required this.onVideoFinished,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -25,7 +31,6 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -42,17 +47,27 @@ class _VideoPostState extends State<VideoPost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child:
-              _videoPlayerController.value.isInitialized
-                  ? VideoPlayer(_videoPlayerController)
-                  : Container(color: Colors.black),
-        ),
-      ],
+    return VisibilityDetector(
+      key: Key("${widget.index}"),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child:
+                _videoPlayerController.value.isInitialized
+                    ? VideoPlayer(_videoPlayerController)
+                    : Container(color: Colors.black),
+          ),
+        ],
+      ),
     );
   }
 }
