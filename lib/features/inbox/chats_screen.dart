90bbd8c189f80604a9d4e5be41bf2899df3bb7bd
iinsue <hinsu93@gmail.com>
2025-05-06 17:29:36 +0900

    #11.2 AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,20 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: Duration(milliseconds: 400),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +32,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
         elevation: 1,
         shadowColor: Colors.grey.shade500,
         actions: [
-          IconButton(onPressed: () {}, icon: FaIcon(FontAwesomeIcons.plus)),
+          IconButton(onPressed: _addItem, icon: FaIcon(FontAwesomeIcons.plus)),
         ],
         title: Row(
           mainAxisSize: MainAxisSize.min,
@@ -27,35 +41,49 @@ class _ChatsScreenState extends State<ChatsScreen> {
         ),
       ),
 
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              radius: 30,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/50567588?v=4",
-              ),
-              child: Text("인수", style: TextStyle(color: Colors.white)),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text("Nico", style: TextStyle(fontWeight: FontWeight.w600)),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    fontSize: Sizes.size12,
-                    color: Colors.grey.shade500,
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: ScaleTransition(
+              scale: animation,
+              child: SizeTransition(
+                sizeFactor: animation,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.deepPurple,
+                    radius: 30,
+                    foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/50567588?v=4",
+                    ),
+                    child: Text("인수", style: TextStyle(color: Colors.white)),
                   ),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Nico ($index)",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "2:16 PM",
+                        style: TextStyle(
+                          fontSize: Sizes.size12,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("Don't forget to make video"),
                 ),
-              ],
+              ),
             ),
-            subtitle: Text("Don't forget to make video"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
