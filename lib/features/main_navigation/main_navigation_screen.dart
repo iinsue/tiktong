6e4608a7ffe60623e144453b3e604bba987a2cba
insu <hinsu93@gmail.com>
2025-05-09 19:57:38 +0900

    #15.7 Dark Mode part Three

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -8,6 +8,7 @@ import 'package:tiktong/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktong/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktong/features/users/user_profile_screen.dart';
 import 'package:tiktong/features/videos/video_timeline_screen.dart';
+import 'package:tiktong/utils.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -38,9 +39,12 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = isDarkMode(context);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: _selectedIndex == 0 ? Colors.black : Colors.white,
+      backgroundColor:
+          _selectedIndex == 0 || isDark ? Colors.black : Colors.white,
       body: Stack(
         children: [
           Offstage(offstage: _selectedIndex != 0, child: VideoTimelineScreen()),
