19a91ed330578fd91beccac81a213e44b3b40244
insu <hinsu93@gmail.com>
2025-05-15 20:40:00 +0900

    #21.2 파일 이동

--- lib/features/videos/views/video_recording_screen.dart
+++ lib/features/videos/views/video_recording_screen.dart
@@ -8,7 +8,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tiktong/constants/gaps.dart';
 import 'package:tiktong/constants/sizes.dart';
-import 'package:tiktong/features/videos/video_preview_screen.dart';
+import 'package:tiktong/features/videos/views/video_preview_screen.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   static const String routeName = 'postVideo';
