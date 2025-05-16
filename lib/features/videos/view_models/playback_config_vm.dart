78e4f3d98a3707d557a20452d47d492c009d7824
insu <hinsu93@gmail.com>
2025-05-16 20:12:19 +0900

    #22.1 NotifierProvider

--- lib/features/videos/view_models/playback_config_vm.dart
+++ lib/features/videos/view_models/playback_config_vm.dart
@@ -1,40 +1,34 @@
 // 파일이름의 vm은 view model의 이니셜입니다.
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktong/features/videos/models/playback_config_model.dart';
 import 'package:tiktong/features/videos/repositories/video_playback_config_repo.dart';
 
-class PlaybackConfigViewModel extends ChangeNotifier {
+class PlaybackConfigViewModel extends Notifier<PlaybackConfigModel> {
   final VideoPlaybackConfigRepository _repository;
-
-  late final PlaybackConfigModel _model = PlaybackConfigModel(
-    muted: _repository.isMuted(),
-    autoplay: _repository.isAutoplay(),
-  );
 
   PlaybackConfigViewModel(this._repository);
 
-  bool get muted => _model.muted;
-
-  bool get autoplay => _model.autoplay;
-
   void setMuted(bool value) {
-    // 기기 저장소에 저장
     _repository.setMuted(value);
-
-    // 데이터 수정
-    _model.muted = value;
-
-    notifyListeners();
+    state = PlaybackConfigModel(muted: value, autoplay: state.autoplay);
   }
 
   void setAutplay(bool value) {
-    // 기기 저장소에 저장
     _repository.setAutoplay(value);
+    state = PlaybackConfigModel(muted: state.muted, autoplay: value);
+  }
 
-    // 데이터 수정
-    _model.autoplay = value;
-
-    notifyListeners();
+  @override
+  PlaybackConfigModel build() {
+    return PlaybackConfigModel(
+      muted: _repository.isMuted(),
+      autoplay: _repository.isAutoplay(),
+    );
   }
 }
+
+final playbackConfigProvider =
+    NotifierProvider<PlaybackConfigViewModel, PlaybackConfigModel>(
+      () => throw UnimplementedError(),
+    );
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tiktong/constants/sizes.dart';
 import 'package:tiktong/features/videos/repositories/video_playback_config_repo.dart';
@@ -22,7 +21,16 @@ void main() async {
   final preferences = await SharedPreferences.getInstance();
   final repository = VideoPlaybackConfigRepository(preferences);
 
-  runApp(ProviderScope(child: TikTongApp()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        playbackConfigProvider.overrideWith(
+          () => PlaybackConfigViewModel(repository),
+        ),
+      ],
+      child: TikTongApp(),
+    ),
+  );
 }
 
 class TikTongApp extends StatelessWidget {
