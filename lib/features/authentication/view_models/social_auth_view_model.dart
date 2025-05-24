2c4cc1c3fe163a0024c61d791231f496d9ea7406
iinsue <hinsu93@gmail.com>
2025-05-25 01:27:38 +0900

    #24.4 Github Login

---
+++ lib/features/authentication/view_models/social_auth_view_model.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tiktong/features/authentication/repositories/authentication_repo.dart';
+import 'package:tiktong/utils.dart';
+
+class SocialAuthViewModel extends AsyncNotifier<void> {
+  late final AuthenticationRepository _repository;
+
+  @override
+  FutureOr<void> build() {
+    _repository = ref.read(authRepo);
+  }
+
+  Future<void> githubSignIn(BuildContext context) async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async => await _repository.githubSignIn(),
+    );
+
+    if (!context.mounted) return;
+
+    if (state.hasError) {
+      showFirebaseErrorSnack(context, state.error);
+    } else {
+      context.go("/home");
+    }
+  }
+}
+
+final socialAuthProvider = AsyncNotifierProvider<SocialAuthViewModel, void>(
+  () => SocialAuthViewModel(),
+);
