c75a389e09cdc668fb5ee92336d8f3e9b5c177b2
iinsue <hinsu93@gmail.com>
2025-06-01 03:27:01 +0900

    #25.6 onAvatarUpload

--- lib/features/users/view_models/avatar_view_model.dart
+++ lib/features/users/view_models/avatar_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktong/features/authentication/repositories/authentication_repo.dart';
 import 'package:tiktong/features/users/repositories/user_repo.dart';
+import 'package:tiktong/features/users/view_models/users_view_model.dart';
 
 class AvatarViewModel extends AsyncNotifier<void> {
   late final UserRepository _repository;
@@ -15,8 +16,13 @@ class AvatarViewModel extends AsyncNotifier<void> {
   Future<void> uploadAvatar(File file) async {
     state = AsyncValue.loading();
     final fileName = ref.read(authRepo).user!.uid;
-    state = await AsyncValue.guard(
-      () async => _repository.uploadAvatar(file, fileName),
-    );
+    state = await AsyncValue.guard(() async {
+      await _repository.uploadAvatar(file, fileName);
+      await ref.read(usersProvider.notifier).onAvatarUpload();
+    });
   }
 }
+
+final avatarProvider = AsyncNotifierProvider<AvatarViewModel, void>(
+  () => AvatarViewModel(),
+);
