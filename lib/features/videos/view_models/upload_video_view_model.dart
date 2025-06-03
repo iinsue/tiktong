1d221ac10bf9f84a0486715d6db3209ac487c308
iinsue <hinsu93@gmail.com>
2025-06-03 20:54:46 +0900

    #26.2 uploadVideoProvider

--- lib/features/videos/view_models/upload_video_view_model.dart
+++ lib/features/videos/view_models/upload_video_view_model.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiktong/features/authentication/repositories/authentication_repo.dart';
+import 'package:tiktong/features/users/view_models/users_view_model.dart';
+import 'package:tiktong/features/videos/models/video_model.dart';
 import 'package:tiktong/features/videos/repositories/videos_repo.dart';
 
 class UploadVideoViewModel extends AsyncNotifier<void> {
@@ -13,16 +17,38 @@ class UploadVideoViewModel extends AsyncNotifier<void> {
     _repository = ref.read(videosRepo);
   }
 
-  Future<void> uplaodVideo(File video) async {
+  Future<void> uploadVideo(File video, BuildContext context) async {
     final user = ref.read(authRepo).user;
+    final userProfile = ref.read(usersProvider).value;
 
-    state = AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
-      final task = await _repository.uploadVideoFile(video, user!.uid);
+    if (userProfile != null) {
+      state = AsyncValue.loading();
+      state = await AsyncValue.guard(() async {
+        final task = await _repository.uploadVideoFile(video, user!.uid);
 
-      if (task.metadata != null) {
-        await _repository.saveVideo();
-      }
-    });
+        if (task.metadata != null) {
+          await _repository.saveVideo(
+            VideoModel(
+              title: "From Flutter!",
+              description: "Hell yeah!",
+              fileUrl: await task.ref.getDownloadURL(),
+              thumbnailUrl: "",
+              likes: 0,
+              comments: 0,
+              creator: userProfile.name,
+              creatorUid: user.uid,
+              createdAt: DateTime.now().millisecondsSinceEpoch,
+            ),
+          );
+
+          if (!context.mounted) return;
+          context.pushReplacement("/home");
+        }
+      });
+    }
   }
 }
+
+final uploadVideoProvider = AsyncNotifierProvider<UploadVideoViewModel, void>(
+  () => UploadVideoViewModel(),
+);
