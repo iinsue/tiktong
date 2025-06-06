40c842aac9b3c159a2c0e11d7bbb1caab13b0fa0
iinsue <hinsu93@gmail.com>
2025-06-06 23:58:43 +0900

    #27.2 Infinite Firebase

--- lib/features/videos/view_models/timeline_view_model.dart
+++ lib/features/videos/view_models/timeline_view_model.dart
@@ -8,15 +8,26 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   late final VideosRepository _repository;
   List<VideoModel> _list = [];
 
+  Future<List<VideoModel>> _fetchVideos({int? lastItemCreatedAt}) async {
+    final result = await _repository.fetchVideos(lastItemCreatedAt: null);
+    final videos = result.docs.map((doc) => VideoModel.fromJson(doc.data()));
+
+    return videos.toList(); //이터러블을 리스트로 전환
+  }
+
   @override
   FutureOr<List<VideoModel>> build() async {
     _repository = ref.read(videosRepo);
-    final result = await _repository.fetchVideos();
-    final newList = result.docs.map((doc) => VideoModel.fromJson(doc.data()));
-
-    _list = newList.toList(); // 이터러블을 리스트로 전환
-
+    _list = await _fetchVideos(lastItemCreatedAt: null);
     return _list;
+  }
+
+  fetchNextPage() async {
+    final nextPage = await _fetchVideos(
+      lastItemCreatedAt: _list.last.createdAt,
+    );
+
+    state = AsyncValue.data([..._list, ...nextPage]);
   }
 }
 
